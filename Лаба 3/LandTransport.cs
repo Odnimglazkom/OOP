def9bee59bb4656969bdcd7dfaccf32a5d7f2fc0
Odnimglazkom <74864381+Odnimglazkom@users.noreply.github.com>
2021-03-26 13:08:18 +0300

    Add files via upload

---
+++ Лаба 3/LandTransport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LandTransport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
