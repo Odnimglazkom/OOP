25d666995a42b78e6bf4b851e4bca9a1b48a0407
Odnimglazkom <74864381+Odnimglazkom@users.noreply.github.com>
2021-03-26 12:57:26 +0300

    Add files via upload

---
+++ Лаба 3/Land/AllTerainBoots.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AllTerainBoots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
